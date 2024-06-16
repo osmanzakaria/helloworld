a7666d6da64f5c954009f9a4910202f1deaef31a
osmanzakaria <osmanabdulkadir283@gmail.com>
2024-06-16 19:30:33 +0300

    classwork.cpp

---
+++ classwork.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Welcome to the wonderful world of c++";
+    cout<<"This is my first output statement";
+    cout<<"Soon i will learn how to output data into my program";
+    return 0;
+}
