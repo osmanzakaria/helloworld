6e05c6e0c6f80b63f5cc71712fae5915fe7c4beb
osmanzakaria <osmanabdulkadir283@gmail.com>
2024-06-16 19:25:59 +0300

    classwork.cpp

---
+++ classwork1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"*****Escape Sequence Practice********";
+    cout<<"BBIT is a univrsity diploma designed to focus on:";
+     cout<<"/t1.Development";
+     cout<<"/t2.Implementation,and";
+     cout<<"/tManagment of information systems.";
+
+     return 0;
+}
---
+++ classwork2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Strathmore University Vision";
+    cout<<"......................";
+    cout<<"To become a leading out-come driven entrepreneurial rsearch university by traslating our excellence into major contribution to culture,economic well-being and quality of life.";
+    
+    return 0;
+
+}
+        
